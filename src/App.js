5b7f2b01e63f5a3e2b2a83ad4d8b192d88deadea
nnnour <nourelaifia@uni.minerva.edu>
2024-07-06 23:01:08 -0700

    Front-End sample with placeholders

--- src/App.js
+++ src/App.js
@@ -5,9 +5,31 @@ function App() {
 
   const url ='https://api.openweathermap.org/data/2.5/weather?q=Tunis&appid=7385a61555191dc559d508832b1685cd'
   return (
-    <div className="App">
-      <h1>Hello</h1>
-     
+    <div className="app">
+      <div className='container'>
+        <div className='top'>
+          <div className='location'>
+            <p>Tunis</p>
+          </div>
+          <div className='temp'>
+            <h1> 75°F</h1>
+          </div>
+          <div className='description'>
+            <p>clouds</p>
+          </div>
+        </div>
+        <div className='bottom'>
+          <div className='feels'>
+            <p>70°F</p>
+          </div>
+          <div className='humidity'>
+            <p>10%</p>
+          </div>
+          <div className='wind'>
+            <p>12 MPH</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
