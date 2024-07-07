ef93d4521d81a7471b143fe3f666327c11a9d93e
nnnour <nourelaifia@uni.minerva.edu>
2024-07-06 21:18:48 -0700

    API configuration

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+
+function App() {
+
+  const url ='https://api.openweathermap.org/data/2.5/weather?q=Tunis&appid=7385a61555191dc559d508832b1685cd'
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+     
+    </div>
+  );
+}
+
+export default App;
